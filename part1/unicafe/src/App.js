dc171187741228bfeb96c8bb907f82546ebfa6f4
tmgasek <tomaszgasek28@gmail.com>
2021-03-22 12:24:58 +0000

    1.7 done

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -8,31 +8,40 @@ const Button = ({ handleClick, text }) => {
   );
 };
 
-const Statistics = ({ good, neutral, bad }) => {
-  return (
-    <div>
-      <h1>Statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-    </div>
-  );
-};
-
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [allClicks, setAll] = useState([]);
 
   const handleGoodClick = () => {
+    setAll(allClicks.concat(1));
     setGood(good + 1);
   };
+
   const handleNeutralClick = () => {
+    setAll(allClicks.concat(0));
     setNeutral(neutral + 1);
   };
+
   const handleBadClick = () => {
+    setAll(allClicks.concat(-1));
     setBad(bad + 1);
+  };
+
+  const calculateAll = () => good + neutral + bad;
+
+  const average = () => {
+    return (
+      allClicks.reduce((a, b) => {
+        return a + b;
+      }, 0) / allClicks.length
+    );
+  };
+
+  const calculatePositive = () => {
+    return (good / calculateAll()) * 100;
   };
 
   return (
@@ -40,7 +49,20 @@ const App = () => {
       <Button handleClick={handleGoodClick} text="good" />
       <Button handleClick={handleNeutralClick} text="neutral" />
       <Button handleClick={handleBadClick} text="bad" />
-      <Statistics good={good} neutral={neutral} bad={bad} />
+      <h1>Statistics</h1>
+      <p>
+        good {good}
+        <br />
+        neutral {neutral}
+        <br />
+        bad {bad}
+        <br />
+        all {calculateAll()}
+        <br />
+        average {average()}
+        <br />
+        pos {calculatePositive()}%
+      </p>
     </div>
   );
 };
