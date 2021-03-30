e6ceb22be0bb6ebdbf8b023f02fcab9dcda0ce82
tmgasek <tomaszgasek28@gmail.com>
2021-03-30 13:08:46 +0100

    phonebook 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const Filter = (props) => {
   return (
@@ -27,7 +28,6 @@ const PersonForm = (props) => {
 };
 
 const Persons = (props) => {
-  console.log(props);
   return (
     <div>
       <h2>Numbers</h2>
@@ -43,15 +43,16 @@ const Persons = (props) => {
 };
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [newFilter, setNewFilter] = useState('');
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons').then((response) => {
+      setPersons(response.data);
+    });
+  }, []);
 
   const addPerson = (e) => {
     e.preventDefault();
