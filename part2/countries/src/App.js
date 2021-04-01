13412fc4ec9143632ec57809cd8c68242333b503
tmgasek <tomaszgasek28@gmail.com>
2021-04-01 13:25:51 +0100

    01

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -5,7 +5,8 @@ const api_key = process.env.REACT_APP_API_KEY;
 const App = () => {
   const [countries, setCountries] = useState([]);
   const [newFilter, setFilter] = useState('');
-  const [weather, setWeather] = useState('');
+  const [weather, setWeather] = useState([]);
+  const [selectedCountry, setSelectedCountry] = useState([]);
 
   useEffect(() => {
     axios
@@ -16,11 +17,13 @@ const App = () => {
   useEffect(() => {
     axios
       .get(
-        `http://api.weatherstack.com/current?access_key=${api_key}&query=New York`
+        `http://api.weatherstack.com/current?access_key=${api_key}&query=Warsaw`
       )
-      .then((response) => setWeather(response.data));
+      .then((response) => {
+        console.log('promise fulfilled');
+        setWeather(response.data);
+      });
   }, []);
-  console.log(weather);
 
   const handleFilterChange = (e) => {
     setFilter(e.target.value);
@@ -30,46 +33,51 @@ const App = () => {
     setFilter(e.target.previousSibling.textContent.toLowerCase());
   };
 
-  const showCountries = () => {
-    const filtered = countries.filter((c) =>
-      c.name.toLowerCase().includes(newFilter.toLowerCase())
-    );
-
-    if (filtered.length > 10) {
-      return <div> too many </div>;
-    }
-    if (filtered.length === 1) {
-      const c = filtered[0];
-      return (
-        <div>
-          <h1>{c.name}</h1>
-          <p>capital {c.capital}</p>
-          <p>population {c.population}</p>
-          <h1>languages</h1>
-          <ul>
-            {c.languages.map((l) => {
-              return <li key={l.name}>{l.name}</li>;
-            })}
-          </ul>
-        </div>
-      );
-    }
-    return filtered.map((c) => {
-      return (
-        <div key={c.alpha3Code}>
-          {c.name}
-          <button onClick={handleSelectClick}>Select</button>
-        </div>
-      );
-    });
-  };
-
   return (
     <div>
       <input value={newFilter} onChange={handleFilterChange} />
-      <div>{showCountries()}</div>
+      <Countries
+        countries={countries}
+        handleSelectClick={handleSelectClick}
+        newFilter={newFilter}
+      />
     </div>
   );
+};
+
+const Countries = (props) => {
+  const countriesFiltered = props.countries.filter((c) =>
+    c.name.toLowerCase().includes(props.newFilter.toLowerCase())
+  );
+
+  if (countriesFiltered.length > 10) {
+    return <div> too many </div>;
+  }
+  if (countriesFiltered.length === 1) {
+    const c = countriesFiltered[0];
+    return (
+      <div>
+        <h1>{c.name}</h1>
+        <p>capital {c.capital}</p>
+        <p>population {c.population}</p>
+        <h1>languages</h1>
+        <ul>
+          {c.languages.map((l) => {
+            return <li key={l.name}>{l.name}</li>;
+          })}
+        </ul>
+        {/* <img src={c.flag} /> */}
+      </div>
+    );
+  }
+  return countriesFiltered.map((c) => {
+    return (
+      <div key={c.alpha3Code}>
+        {c.name}
+        <button onClick={props.handleSelectClick}>Select</button>
+      </div>
+    );
+  });
 };
 
 export default App;
