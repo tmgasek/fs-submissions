0c3fcce6f101ee85bd595a0d767866ac2dc68370
tmgasek <tomaszgasek28@gmail.com>
2021-04-06 10:07:44 +0100

    seperated components

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,84 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-const api_key = process.env.REACT_APP_API_KEY;
-
-const Weather = ({ capital }) => {
-  const [weatherDetails, setWeatherDetails] = useState(null);
-
-  useEffect(() => {
-    axios
-      .get('http://api.weatherstack.com/current', {
-        params: {
-          access_key: api_key,
-          query: capital,
-        },
-      })
-      .then((response) => {
-        setWeatherDetails(response.data);
-      });
-  }, [capital]);
-  console.log('weather', weatherDetails);
-
-  return (
-    <div>
-      <h3>Weather in {capital} </h3>
-      <p>
-        {' '}
-        temperature: {weatherDetails && weatherDetails.current.temperature}{' '}
-        celcius
-      </p>
-      <img
-        src={weatherDetails && weatherDetails.current.weather_icons[0]}
-        alt=""
-      />
-      <p>
-        wind: {weatherDetails && weatherDetails.current.wind_speed} mph
-        direction {weatherDetails && weatherDetails.current.wind_dir}{' '}
-      </p>
-    </div>
-  );
-};
-
-const Countries = (props) => {
-  const countriesFiltered = props.countries.filter((c) =>
-    c.name.toLowerCase().includes(props.newFilter.toLowerCase())
-  );
-
-  if (countriesFiltered.length > 10) {
-    return <div> too many </div>;
-  }
-  if (countriesFiltered.length === 1) {
-    const c = countriesFiltered[0];
-
-    return (
-      <div>
-        <h1>{c.name}</h1>
-        <p>capital {c.capital}</p>
-        <p>population {c.population}</p>
-        <h1>languages</h1>
-        <ul>
-          {c.languages.map((l) => {
-            return <li key={l.name}>{l.name}</li>;
-          })}
-        </ul>
-        <img src={c.flag} className="flag" alt="" />
-        <Weather capital={c.capital} />
-      </div>
-    );
-  }
-  return countriesFiltered.map((c) => {
-    return (
-      <div key={c.alpha3Code}>
-        {c.name}
-        <button onClick={props.handleSelectClick}>Select</button>
-      </div>
-    );
-  });
-};
+import { Countries } from './components/Countries';
 
 const App = () => {
   const [countries, setCountries] = useState([]);
   const [newFilter, setFilter] = useState('');
+  const [weatherDetails, setWeatherDetails] = useState(null);
 
   useEffect(() => {
     axios
@@ -101,6 +28,8 @@ const App = () => {
         countries={countries}
         handleSelectClick={handleSelectClick}
         newFilter={newFilter}
+        weatherDetails={weatherDetails}
+        setWeatherDetails={setWeatherDetails}
       />
     </div>
   );
