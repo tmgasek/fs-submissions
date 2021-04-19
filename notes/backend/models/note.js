7c12da1484b2045f48f9dec9933352a9353384b0
tmgasek <tomaszgasek28@gmail.com>
2021-04-19 22:29:36 +0100

    4a

---
+++ notes/backend/models/note.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const config = require('../utils/config');
+
+console.log(`connectfing to ${config.MONGODB_URI}`);
+
+mongoose
+  .connect(config.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log('connected to mongoDB');
+  })
+  .catch((err) => {
+    console.log('error connecting to MongoDB', err.message);
+  });
+
+const noteSchema = new mongoose.Schema({
+  content: {
+    type: String,
+    minlength: 5,
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+  important: Boolean,
+});
+
+noteSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+module.exports = mongoose.model('Note', noteSchema);
