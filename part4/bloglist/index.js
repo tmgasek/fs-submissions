8961bf28bab83d09be808926241d4477f573eaac
tmgasek <tomaszgasek28@gmail.com>
2021-04-22 14:21:20 +0100

    4.2

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,66 +1,10 @@
-const express = require('express');
-const app = express();
-const cors = require('cors');
-const mongoose = require('mongoose');
-require('dotenv').config();
+const app = require('./app');
+const http = require('http');
+const config = require('./utils/config');
+const logger = require('./utils/logger');
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-});
+const server = http.createServer(app);
 
-const Blog = mongoose.model('Blog', blogSchema);
-
-mongoose
-  .connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
-  })
-  .then(() => {
-    console.log('connected to mongoDB');
-  })
-  .catch((error) =>
-    console.log(`error connecting to mongoDB ${error.message}`)
-  );
-
-app.use(cors);
-app.use(express.json());
-
-app.get('/api/blogs', (request, response, next) => {
-  Blog.find({})
-    .then((blogs) => {
-      response.json(blogs);
-    })
-    .catch((error) => next(error));
-});
-
-app.post('/api/blogs', (request, response, next) => {
-  const blog = new Blog(request.body);
-
-  blog
-    .save()
-    .then((result) => {
-      response.status(201).json(result);
-    })
-    .catch((error) => next(error));
-});
-
-const errorHandler = (error, request, response, next) => {
-  console.log(error(error.message));
-  if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' });
-  } else if (error.name === 'ValidationError') {
-    return response.status(400).json({ error: error.message });
-  }
-  next(error);
-};
-
-app.use(errorHandler);
-
-app.listen(process.env.PORT, () => {
-  console.log(`server running on port ${process.env.PORT}`);
+server.listen(config.PORT, () => {
+  logger.info(`server running on port ${config.PORT}`);
 });
