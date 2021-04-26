af1fc87f32d8bc280f631b43cf592cad003aa007
tmgasek <tomaszgasek28@gmail.com>
2021-04-26 14:54:32 +0100

    4.4

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,7 +1,19 @@
-const dummy = (blogs) => {
-  return 1;
+// const dummy = (blogs) => {
+//   return 1;
+// };
+
+// module.exports = {
+//   dummy,
+// };
+
+const totalLikes = (blogs) => {
+  if (!blogs) return 0;
+  return blogs.reduce((accumulator, current) => {
+    return accumulator + current.likes;
+  }, 0);
 };
 
 module.exports = {
-  dummy,
+  // dummy,
+  totalLikes,
 };
