dc54ea28e4bc7e4dbc78248994178a5bcb15ea84
tmgasek <tomaszgasek28@gmail.com>
2021-04-28 15:03:00 +0100

    async error handling

--- notes/backend/controllers/notes.js
+++ notes/backend/controllers/notes.js
@@ -1,10 +1,9 @@
 const notesRouter = require('express').Router();
 const Note = require('../models/note');
 
-notesRouter.get('/', (request, response) => {
-  Note.find({}).then((notes) => {
-    response.json(notes);
-  });
+notesRouter.get('/', async (request, response) => {
+  const notes = await Note.find({});
+  response.json(notes);
 });
 
 notesRouter.get('/:id', (request, response, next) => {
@@ -19,7 +18,7 @@ notesRouter.get('/:id', (request, response, next) => {
     .catch((error) => next(error));
 });
 
-notesRouter.post('/', (request, response, next) => {
+notesRouter.post('/', async (request, response) => {
   const body = request.body;
 
   const note = new Note({
@@ -28,12 +27,14 @@ notesRouter.post('/', (request, response, next) => {
     date: new Date(),
   });
 
-  note
-    .save()
-    .then((savedNote) => {
-      response.json(savedNote);
-    })
-    .catch((error) => next(error));
+  // note
+  //   .save()
+  //   .then((savedNote) => {
+  //     response.json(savedNote);
+  //   })
+  //   .catch((error) => next(error));
+  const savedNote = await note.save();
+  response.json(savedNote);
 });
 
 notesRouter.delete('/:id', (request, response, next) => {
--- notes/backend/tests/note_api.test.js
+++ notes/backend/tests/note_api.test.js
@@ -1,8 +1,17 @@
 const mongoose = require('mongoose');
 const supertest = require('supertest');
+const helper = require('./test_helper');
 const app = require('../app');
-
 const api = supertest(app);
+const Note = require('../models/note');
+
+beforeEach(async () => {
+  await Note.deleteMany({});
+  let noteObject = new Note(helper.initialNotes[0]);
+  await noteObject.save();
+  noteObject = new Note(helper.initialNotes[1]);
+  await noteObject.save();
+});
 
 test('notes are returned as json', async () => {
   await api
@@ -11,17 +20,55 @@ test('notes are returned as json', async () => {
     .expect('Content-Type', /application\/json/);
 });
 
-test('there are 10 notes', async () => {
+test('all notes are returned', async () => {
   const response = await api.get('/api/notes');
 
-  //the execution gets here only after the HTTP req is complete.
-  //the result of HTTP req is saved in variable response.
-  expect(response.body).toHaveLength(2);
+  expect(response.body).toHaveLength(helper.initialNotes.length);
 });
 
-test('second note says two', async () => {
+test('a specific note is within the returned notes', async () => {
   const response = await api.get('/api/notes');
-  expect(response.body[1].content).toBe('two');
+
+  const contents = response.body.map((r) => r.content);
+  expect(contents).toContain('Browser can execute only Javascript');
+});
+
+test('a valid note can be added', async () => {
+  const newNote = {
+    content: 'async/await test',
+    important: true,
+  };
+
+  await api
+    .post('/api/notes')
+    .send(newNote)
+    .expect(200)
+    .expect('Content-Type', /application\/json/);
+
+  // const response = await api.get('/api/notes');
+  // const contents = response.body.map((r) => r.content);
+
+  // expect(response.body).toHaveLength(helper.initialNotes.length + 1);
+  // expect(contents).toContain('async/await test');
+
+  const notesAtEnd = await helper.notesInDb();
+  expect(notesAtEnd).toHaveLength(helper.initialNotes.length + 1);
+
+  const contents = notesAtEnd.map((n) => n.content);
+  expect(contents).toContain('async/await test');
+});
+
+test('note without content is not added', async () => {
+  const newNote = {
+    important: true,
+  };
+
+  await api.post('/api/notes').send(newNote).expect(400);
+
+  // const response = await api.get('/api/notes');
+  // expect(response.body).toHaveLength(helper.initialNotes.length);
+  const notesAtEnd = await helper.notesInDb();
+  expect(notesAtEnd).toHaveLength(helper.initialNotes.length);
 });
 
 afterAll(() => {
