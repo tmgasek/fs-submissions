d1ef6e4e07276ed4cf1e8326da0a36a0f4919c97
tmgasek <tomaszgasek28@gmail.com>
2021-04-29 10:41:45 +0100

    working 4.4

--- notes/backend/tests/note_api.test.js
+++ notes/backend/tests/note_api.test.js
@@ -45,12 +45,6 @@ test('a valid note can be added', async () => {
     .expect(200)
     .expect('Content-Type', /application\/json/);
 
-  // const response = await api.get('/api/notes');
-  // const contents = response.body.map((r) => r.content);
-
-  // expect(response.body).toHaveLength(helper.initialNotes.length + 1);
-  // expect(contents).toContain('async/await test');
-
   const notesAtEnd = await helper.notesInDb();
   expect(notesAtEnd).toHaveLength(helper.initialNotes.length + 1);
 
@@ -65,10 +59,34 @@ test('note without content is not added', async () => {
 
   await api.post('/api/notes').send(newNote).expect(400);
 
-  // const response = await api.get('/api/notes');
-  // expect(response.body).toHaveLength(helper.initialNotes.length);
   const notesAtEnd = await helper.notesInDb();
   expect(notesAtEnd).toHaveLength(helper.initialNotes.length);
+});
+
+test('a specific note can be viewed', async () => {
+  const notesAtStart = await helper.notesInDb();
+  const noteToView = notesAtStart[0];
+
+  const resultNote = await api
+    .get(`/api/notes/${noteToView.id}`)
+    .expect(200)
+    .expect('Content-Type', /application\/json/);
+
+  const processedNoteToView = JSON.parse(JSON.stringify(noteToView));
+  expect(resultNote.body).toEqual(processedNoteToView);
+});
+
+test('a note can be deleted', async () => {
+  const notesAtStart = await helper.notesInDb();
+  const noteToDelete = notesAtStart[0];
+
+  await api.delete(`/api/notes/${noteToDelete.id}`).expect(204);
+
+  const notesAtEnd = await helper.notesInDb();
+  expect(notesAtEnd).toHaveLength(helper.initialNotes.length - 1);
+
+  const contents = notesAtEnd.map((r) => r.content);
+  expect(contents).not.toContain(noteToDelete.content);
 });
 
 afterAll(() => {
