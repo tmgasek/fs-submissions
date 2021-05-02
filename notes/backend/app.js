adb8ce8bc7ded7276075896a833d732da8516087
tmgasek <tomaszgasek28@gmail.com>
2021-05-02 19:14:07 +0100

    user administration

--- notes/backend/app.js
+++ notes/backend/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 require('express-async-errors');
 const app = express();
 const cors = require('cors');
+const usersRouter = require('./controllers/users');
 const notesRouter = require('./controllers/notes');
 const middleware = require('./utils/middleware');
 const logger = require('./utils/logger');
@@ -29,6 +30,7 @@ app.use(express.static('build'));
 app.use(express.json());
 app.use(middleware.requestLogger);
 
+app.use('/api/users', usersRouter);
 app.use('/api/notes', notesRouter);
 
 app.use(middleware.unknownEndpoint);
