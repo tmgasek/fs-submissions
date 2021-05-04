482b9b66d53dbb14ca72b1bfd33e6bf2dbe8b78b
tmgasek <tomaszgasek28@gmail.com>
2021-05-04 13:03:30 +0100

    4.17

--- part4/bloglist/controllers/users.js
+++ part4/bloglist/controllers/users.js
@@ -3,7 +3,12 @@ const User = require('../models/user');
 const bcrypt = require('bcrypt');
 
 usersRouter.get('/', async (request, response) => {
-  const users = await User.find({});
+  const users = await User.find({}).populate('blogs', {
+    url: 1,
+    title: 1,
+    author: 1,
+    id: 1,
+  });
   response.json(users);
 });
 
