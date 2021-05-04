5917d35693fcba3f0759326dddccb02ba2fddc5c
tmgasek <tomaszgasek28@gmail.com>
2021-05-04 08:05:28 +0100

    part4end

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -3,7 +3,10 @@ const express = require('express');
 require('express-async-errors');
 const app = express();
 const cors = require('cors');
+
+const usersRouter = require('./controllers/users');
 const blogsRouter = require('./controllers/blogs');
+
 const middleware = require('./utils/middleware');
 const logger = require('./utils/logger');
 const mongoose = require('mongoose');
@@ -28,6 +31,7 @@ app.use(cors());
 app.use(express.json());
 app.use(middleware.requestLogger);
 
+app.use('/api/users', usersRouter);
 app.use('/api/blogs', blogsRouter);
 
 app.use(middleware.errorHandler);
---
+++ part4/bloglist/controllers/users.js
@@ -0,0 +1,32 @@
+const usersRouter = require('express').Router();
+const User = require('../models/user');
+const bcrypt = require('bcrypt');
+
+usersRouter.get('/', async (request, response) => {
+  const users = await User.find({});
+  response.json(users);
+});
+
+usersRouter.post('/', async (request, response) => {
+  const body = request.body;
+
+  if (!body.password) {
+    response.status(401).json({
+      error: 'wrong username/pass',
+    });
+  }
+
+  const saltRounds = 10;
+  const passwordHash = await bcrypt.hash(body.password, saltRounds);
+
+  const user = new User({
+    username: body.username,
+    name: body.name,
+    passwordHash,
+  });
+
+  const savedUser = await user.save();
+  response.json(savedUser);
+});
+
+module.exports = usersRouter;
