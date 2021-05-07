f9e5a2f25d9c27e6be20ca142e31372bdf4900d8
tmgasek <tomaszgasek28@gmail.com>
2021-05-07 15:00:40 +0100

    refactored login note forms

--- notes/frontend/src/services/notes.js
+++ notes/frontend/src/services/notes.js
@@ -1,6 +1,12 @@
 import axios from 'axios';
 const baseUrl = '/api/notes';
 
+let token = null;
+
+const setToken = (newToken) => {
+  token = `bearer ${newToken}`;
+};
+
 const getAll = () => {
   const request = axios.get(baseUrl);
   return request.then((response) => response.data);
