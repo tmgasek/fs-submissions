2e531ad2540efd178d14ee9bcce1189349c4a17a
tmgasek <tomaszgasek28@gmail.com>
2021-05-13 10:46:35 +0100

    5.7

--- part5/bloglist/frontend/src/components/Blog.js
+++ part5/bloglist/frontend/src/components/Blog.js
@@ -1,8 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+  const [initialView, setInitialView] = useState(true);
+
+  const handleClick = () => {
+    setInitialView(!initialView);
+  };
+
+  const simplerView = () => {
+    return (
+      <div style={blogStyle}>
+        {blog.title} {blog.author}
+        <button onClick={handleClick}>view details</button>
+      </div>
+    );
+  };
+
+  const detailedView = () => {
+    return (
+      <div style={blogStyle}>
+        {blog.title} {blog.author}
+        <br /> {blog.url}
+        <br />
+        {blog.likes} <br />
+        {blog.user.username}
+        <button onClick={handleClick}>view simple</button>
+      </div>
+    );
+  };
+
   return (
     <div>
-      {blog.title} {blog.author}
+      {initialView === false ? (
+        <div>{detailedView()} </div>
+      ) : (
+        <div>{simplerView()} </div>
+      )}
     </div>
   );
 };
