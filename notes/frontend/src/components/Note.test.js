b75aa514a869d487c00e4ac36252184bca6c75dd
tmgasek <tomaszgasek28@gmail.com>
2021-05-17 15:06:01 +0100

    testing

---
+++ notes/frontend/src/components/Note.test.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import '@testing-library/jest-dom/extend-expect';
+import { render, fireEvent } from '@testing-library/react';
+import { prettyDOM } from '@testing-library/dom';
+import Note from './Note';
+
+test('renders content', () => {
+  const note = {
+    content: 'test content',
+    important: true,
+  };
+  const component = render(<Note note={note} />);
+  const li = component.container.querySelector('li');
+  // component.debug();
+  // console.log(prettyDOM(li));
+  expect(component.container).toHaveTextContent('test content');
+});
+
+test('clicking the button calls event handler once', () => {
+  const note = {
+    content: 'testing button',
+    important: true,
+  };
+  const mochHandler = jest.fn();
+  const component = render(<Note note={note} toggleImportance={mochHandler} />);
+  const button = component.getByText('make not important');
+  fireEvent.click(button);
+  expect(mochHandler.mock.calls).toHaveLength(1);
+});
