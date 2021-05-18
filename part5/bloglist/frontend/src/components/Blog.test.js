22298e73baa09e26d27f6dcd1bf0b28326c0cc7a
tmgasek <tomaszgasek28@gmail.com>
2021-05-18 13:27:00 +0100

    5.13

---
+++ part5/bloglist/frontend/src/components/Blog.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '@testing-library/jest-dom/extend-expect';
+import { render } from '@testing-library/react';
+import Blog from './Blog';
+
+test('renders content', () => {
+  const blog = {
+    title: 'test title',
+    author: 'test author',
+    url: 'test url',
+    likes: 5,
+  };
+
+  const component = render(<Blog blog={blog} />);
+  expect(component.container).toHaveTextContent('test title');
+  expect(component.container).not.toHaveTextContent('5');
+  expect(component.container).not.toHaveTextContent('test url');
+});
