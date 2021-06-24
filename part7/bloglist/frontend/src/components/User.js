9f0446a651a9b605e15a7d33939fcff5c297c071
tmgasek <tomaszgasek28@gmail.com>
2021-06-24 14:15:50 +0100

    7.14

--- part7/bloglist/frontend/src/components/User.js
+++ part7/bloglist/frontend/src/components/User.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import { useParams } from 'react-router';
+import { useSelector } from 'react-redux';
 
 const User = () => {
+  const id = useParams().id;
+  const users = useSelector((state) => state.users);
+
+  const user = users.find((user) => user.id === id);
+
+  if (!user) {
+    return null;
+  }
+
   return (
     <div>
-      <h2>Hello</h2>
-      <div></div>
+      <h2>{user.name}</h2>
+      <h3>added blogs</h3>
+      <ul>
+        {user.blogs.map((blog) => (
+          <li key={blog.id}>{blog.title}</li>
+        ))}
+      </ul>
     </div>
   );
 };
