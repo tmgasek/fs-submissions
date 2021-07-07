5c6037060fd6c63de7c1984bb02fe751a3511d09
tmgasek <tomaszgasek28@gmail.com>
2021-07-07 13:57:45 +0100

    error handling

---
+++ part8/phonebook-frontend-example/src/App.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import Persons from './components/Persons';
+import { useQuery } from '@apollo/client';
+import PersonForm from './components/PersonForm';
+import { ALL_PERSONS } from './queries';
+
+const App = () => {
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  const result = useQuery(ALL_PERSONS);
+
+  if (result.loading) {
+    return <div>loading...</div>;
+  }
+
+  const notify = (message) => {
+    setErrorMessage(message);
+    setTimeout(() => {
+      setErrorMessage(null);
+    }, 5000);
+  };
+
+  return (
+    <div>
+      <Notify errorMessage={errorMessage} />
+      <Persons persons={result.data.allPersons} />
+      <PersonForm setError={notify} />
+    </div>
+  );
+};
+
+const Notify = ({ errorMessage }) => {
+  if (!errorMessage) {
+    return null;
+  }
+  return <div style={{ color: 'red' }}>{errorMessage}</div>;
+};
+
+export default App;
