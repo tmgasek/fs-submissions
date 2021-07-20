3c831dfc55f31bf5d299826d339e2bd26b1180c4
tmgasek <tomaszgasek28@gmail.com>
2021-07-20 18:49:20 +0100

    8.19

--- part8/library-frontend/src/components/Books.js
+++ part8/library-frontend/src/components/Books.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 
 const Books = ({ show, result }) => {
-  const [filter, setFilter] = useState('');
+  const [filter, setFilter] = useState('All genres');
   if (!show) {
     return null;
   }
@@ -20,28 +20,51 @@ const Books = ({ show, result }) => {
   const handleFilterClick = (genre) => {
     console.log(genre);
     setFilter(genre);
+  };
 
-    // const filtered = allBooks.filter()
+  const filtered = () => {
+    const filtered = allBooks.filter((book) => book.genres.includes(filter));
+    console.log(filtered);
+    return (
+      <>
+        {filtered.map((a) => (
+          <tr key={a.title}>
+            <td>{a.title}</td>
+            <td>{a.author.name}</td>
+            <td>{a.published}</td>
+            <td>{a.genres.join(', ')}</td>
+          </tr>
+        ))}
+      </>
+    );
   };
 
   return (
     <div>
       <h2>books</h2>
-
+      {filter === 'All genres' ? (
+        <p>all genres: </p>
+      ) : (
+        <p>in genre "{filter}":</p>
+      )}
       <table>
         <tbody>
           <tr>
-            <th></th>
+            <th>title</th>
             <th>author</th>
             <th>published</th>
+            <th>genres</th>
           </tr>
-          {allBooks.map((a) => (
-            <tr key={a.title}>
-              <td>{a.title}</td>
-              <td>{a.author.name}</td>
-              <td>{a.published}</td>
-            </tr>
-          ))}
+          {filter === 'All genres'
+            ? allBooks.map((a) => (
+                <tr key={a.title}>
+                  <td>{a.title}</td>
+                  <td>{a.author.name}</td>
+                  <td>{a.published}</td>
+                  <td>{a.genres.join(', ')}</td>
+                </tr>
+              ))
+            : filtered()}
         </tbody>
       </table>
       {genres.map((g) => (
